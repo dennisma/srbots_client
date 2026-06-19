bef090640ee3090a5234090bffcb3bd7ec4886ed
dennis.mabrey <denbo68@gmail.com>
2026-06-19 19:43:02 -0400

    Motor test

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
-#include <Arduino.h>
-#include "RobotSetup.hpp"
+#define BLUE_MOTORS
 
+#include "RobotSetup.hpp"
 using namespace srbots;
 
 void setup() {
-  RobotSetup();
   Serial.begin(115200);
+  RobotSetup();
 }
 
+int speed=60;
+
 void loop() {
-  Serial.println("ugh");
+  Serial.println("Yo");
+  speed = -speed;
+  leftmotor.speed(speed);
+  rightmotor.speed(speed);
+  delay(3000);
 }
